8af4c600f7e1dfa40b64e622ede850a5f82a35d0
Carlos-Anzola <anzolacarlos08@gmail.com>
2022-09-07 13:09:29 -0400

    added firebase

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDPbpbc5lVZgcHM1H58422Enj1HAuGHOCU",
+  authDomain: "ecommerce-5a5e3.firebaseapp.com",
+  projectId: "ecommerce-5a5e3",
+  storageBucket: "ecommerce-5a5e3.appspot.com",
+  messagingSenderId: "300429810935",
+  appId: "1:300429810935:web:434ee41f9841ba26a087f2",
+  measurementId: "G-XQV7FVSJ75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function App() {
   return (
     <div className="App">
